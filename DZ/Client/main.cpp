e33b9ca23f52c045a83915b0a360d839aea0e629
DianaBespechnaya <dianabespechnaya@yandex.ru>
2016-12-13 00:53:23 +0300

    Исправлена работа клиента

--- DZ/Client/main.cpp
+++ DZ/Client/main.cpp
@@ -8,93 +8,65 @@
 
 using boost::asio::ip::tcp;
 
-enum { max_length = 1024 };
-
 int main(int argc, char* argv[])
 {
-    try
-    {
-        if (argc != 3)
-        {
-            std::cerr << "Usage: blocking_tcp_echo_client <host> <port>\n";
-            return 1;
-        }
+   	try
+	{
+		boost::asio::io_service io_service;
 
-        boost::asio::io_service io_service;
+		tcp::resolver resolver(io_service);
+		tcp::resolver::query query(tcp::v4(), argv[1], argv[2]);
+		tcp::resolver::iterator iterator = resolver.resolve(query);
 
-        tcp::resolver resolver(io_service);
-        tcp::resolver::query query(tcp::v4(), argv[1], argv[2]);
-        tcp::resolver::iterator iterator = resolver.resolve(query);
+		tcp::socket s(io_service);
+		boost::asio::connect(s, iterator);
 
-        tcp::socket s(io_service);
-        boost::asio::connect(s, iterator);
+		auto reply = std::make_unique<char[]>(max_length);
 
-        using namespace std;
-        for(;;){
-            char reply[max_length];
-            auto buf = make_shared<boost::asio::streambuf>();
-            boost::asio::read_until(s, *buf, '\n');
-            std::istream stream(buf.get());
-             stream.getline(reply, max_length);
-            std::cout << "Data recieve " << reply << std::endl;
-            string str= reply;
-            char *request = new char[max_length]();
+		for (;;){
+			string request;
+			auto buf = make_unique<boost::asio::streambuf>();
+			boost::asio::read_until(s, *buf, '\n');
+			istream stream(buf.get());
+			getline(stream, request);
+			cout << "Data recieve " << request << endl;
 
-            BOOST_SCOPE_EXIT((request)){
-                delete[] request;
-           } BOOST_SCOPE_EXIT_END
+			if (isdigit(request[0])) {
+				string ch = "";
+				string st = "";
+				auto found = request.find('^');
+				ch = request.substr(0, found);
+				st = request.substr(found + 1, request.size() - found);
 
-            if (reply[0]<=57 && reply[0] > 48) {
-                string ch = "";
-                string st = "";
-                auto found = str.find('^');
-                ch = str.substr(0, found);
-                st = str.substr(found + 1, str.size() - found);
+				mpz_t base;
+				mpz_t pow;
+				mpz_t rez;
+				mpz_init(rez);
+				mpz_init(pow);
+				mpz_init(base);
+				mpz_set_str(pow, st.c_str(), 10);
+				mpz_set_str(base, ch.c_str(), 10);
 
-                mpz_class ch1;
-                mpz_class st1;
-                mpz_class rez;
-                mpz_init(rez.get_mpz_t());
-                mpz_init(st1.get_mpz_t());
-                mpz_init(ch1.get_mpz_t());
-                mpz_set_str(st1.get_mpz_t(), st.c_str(), 10);
-                mpz_set_str(ch1.get_mpz_t(), ch.c_str(), 10);
+				mpz_pow_ui(rez, base, mpz_get_ui(pow));
+				mpz_get_str(reply.get(), 10, rez);
+			}
+			else {
+				reply[0] = '0';
+			}
 
-                rez = 1;
-                for (int i = 1; i <= st1; i++) {
-                    rez = rez * ch1;
-                }
+			string rezult = reply.get();
+			int length = rezult.size();
+			reply[length] = '\n';
 
-                mpz_get_str(request, 10, rez.get_mpz_t());
-            }
-            else {
-                request[0] = '0';
-            }
+			boost::asio::write(s, boost::asio::buffer(reply.get(), length + 1));
 
-	           std::stringstream ss(request);
-            while(ss){
-                char* temp= new char[100]();
-
-                ss.read(temp,100);
-                string temp_str=temp;
-                int length =temp_str.size();
-                if (length < 100) {
-                    temp[length] = '\n';
-                    boost::asio::write(s, boost::asio::buffer(temp,length+1));
-                }
-                else
-                    boost::asio::write(s, boost::asio::buffer(temp,100));
-                delete[] temp;
-            }
-           
-
-            if (request[0]=='0')
-                break;
-        }
-    }
-    catch (std::exception& e)
-    {
-        std::cerr << "Exception: " << e.what() << "\n";
-    }
-     return 0;
+			if (reply[0] == '0')
+				break;
+		}
+	}
+	catch (std::exception& e)
+	{
+		std::cout << "Exception: " << e.what() << "\n";
+	}
+	return 0;
  }
